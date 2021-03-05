bca2b55c7b38b7f49e79d673e0db586ef0c2195b
hieu1999xl <hiu99xl@gmail.com>
2021-03-05 14:10:50 +0700

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,23 @@ $('.set-bg').each(function () {
     var bg = $(this).data('setbg');
     $(this).css('background-image', 'url(' + bg + ')');
 });
+
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
